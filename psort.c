11f6df0ae39e249fa1a9db310fb704e45236777f
Maxime Lawson <lawsonmaxime20@gmail.com>
2025-12-05 10:55:33 +0100

    ajout du code du prof

---
+++ psort.c
@@ -0,0 +1,27 @@
+/***********************************************
+ * Projet 4 : Tri parallèle
+ * 
+ * Numéro de groupe : 3
+ * Étudiants     : Maxime Lawson , Thomas Colangelo
+ * 
+ * Si vous utilisez plusieurs fichiers, expliquez ici comment compiler votre
+ * programme :
+ * 
+ * 
+ *
+ * Expliquez ici le fonctionnement de votre algorithme de tri,
+ * et comment il est parallélisé (mentionnez vos sources si pertinent) :
+ * 
+ * 
+ * 
+ * 
+ * 
+ *
+ ***********************************************/
+
+ // TODO : Votre code ici
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
